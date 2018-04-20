a6feeba8ecaa974bcb8dce0bf598996a7d86639d
Somant <somant@gmail.com>
2018-04-20 23:13:29 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose= require('mongoose');
 
 mongoose.Promise=global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://Somant:1234@ds149268.mlab.com:49268/todoapp'
+  };
+mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
 module.exports={mongoose};
